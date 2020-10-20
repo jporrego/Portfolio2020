c23712024dd1f460d7f3676182bb3d06b3d1a6f2
Juan Pablo Orrego <jporregoc@gmail.com>
2020-10-19 21:47:48 -0300

    v0.05 - many many animations

--- portfolio2020/src/components/pages/projects/Projects.js
+++ portfolio2020/src/components/pages/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../../navbar/Navbar.js';
 import ProjectCard from './ProjectCard.js';
 import { motion, useAnimation } from "framer-motion";
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHtml5, faCss3Alt, faJsSquare, faReact,faNodeJs } from '@fortawesome/free-brands-svg-icons'
 
 function Projects() {
+    const [title, setTitle] = useState("");
+
     useEffect(() => {
         generalAnimation();
       }, []);
@@ -20,9 +22,9 @@ function Projects() {
           });
 
         await titleAnimation.start({
-          y: ["-20%", "0%"], 
+          y: ["-10%", "0%"], 
           opacity: ["0%", "100%"],
-          transition: { duration: .5 }
+          transition: { duration: .4 }
         });
 
         await skillsAnimation.start({
@@ -38,19 +40,40 @@ function Projects() {
     const skillsAnimation = useAnimation();
 
     const variants = {
-      visible: { opacity: 1, transition: {
+      visible: { opacity: 1, y:"0%", transition: {
         when: "beforeChildren",
         staggerChildren: 0.2,
       }, },
-      hidden: { opacity: 0 },
+      hidden: { opacity: 0, y:"-10%"  },
     }
+
+    const writeVariants = {
+      visible: { opacity: 1, y:"0%", transition: {
+        when: "beforeChildren",
+        staggerChildren: .05,
+        ease:"anticipate",
+        delay: .7
+      }, },
+      hidden: { opacity: 0, y:"-5%" },
+    }
+
 
     return (
         <div className="projects">
             <Navbar selected="projects"></Navbar>
             <motion.div className="projects__title" animate={titleAnimation} initial={{opacity:"0%"}}>
               <span style={{display:"inline"}}>&#60;/</span>
-                projects
+              <motion.span className="projects__title__inner-text" initial="hidden" animate="visible" variants={writeVariants}>
+                  <motion.span variants={variants}>P</motion.span> 
+                  <motion.span variants={variants}>R</motion.span> 
+                  <motion.span variants={variants}>O</motion.span> 
+                  <motion.span variants={variants}>J</motion.span> 
+                  <motion.span variants={variants}>E</motion.span> 
+                  <motion.span variants={variants}>C</motion.span>
+                  <motion.span variants={variants}>T</motion.span> 
+                  <motion.span variants={variants}>S</motion.span>
+                  <motion.span id="cursor" animate={{opacity:["0%", "100%", "100%", "100%", "0%"]}} transition={{repeat: Infinity, duration:.6, repeatDelay: .5, ease: "easeIn", delay:1.8}}>|</motion.span>  
+                </motion.span> 
               <span style={{display:"inline"}}>&#62;</span>
               </motion.div>
             <motion.div className="projects__skills" /*animate={skillsAnimation}*/ initial="hidden" animate="visible" variants={variants}>
