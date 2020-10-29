f182fc474f3ba8560ddaa89ba498819c1086c7fc
Juan Pablo Orrego <jporregoc@gmail.com>
2020-10-29 13:23:52 -0300

    v0.06 - Network Anim

--- portfolio2020/src/components/pages/home/Home.js
+++ portfolio2020/src/components/pages/home/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {motion, useViewportScroll, useTransform, useElementScroll } from 'framer-motion';
 import Navbar from '../../navbar/Navbar.js';
 import Landing from '../../landing/Landing.js';
 import Network from '../../network/Network.js'
-
 import './Home.css';
+
+const MotionNetwork = motion.custom(Network);
+
+
 
 function Home() {
     return (
         <div className="home">
             <Navbar></Navbar>
             <Landing></Landing>
-            <Network></Network>
+            <Network ></Network>
+            
         </div>
     )
 }
